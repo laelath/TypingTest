c6e0ecb3a7b5c96fa54819c70bf6cd859d438a6b
Justin Frank <jf.laelath@gmail.com>
2017-01-17 19:22:13 -0800

    added gresources

--- src/main.cpp
+++ src/main.cpp
@@ -226,7 +226,8 @@ int main(int argc, char *argv[])
 	loadConfig();
 
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "us.laelath.typingtest");
-	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("ui/typingui.glade");
+	Glib::RefPtr<Gtk::Builder> builder =
+		Gtk::Builder::create_from_resource("/us/laelath/typingtest/ui/typingui.glade");
 
 	//Prepare main window
 	builder->get_widget("typingtest", appWindow);
